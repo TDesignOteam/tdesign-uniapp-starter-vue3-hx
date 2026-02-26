5cb3cfb352377baa39fc69ce16dc16510297fca9
novlan1 <1576271227@qq.com>
2026-02-26 17:21:08 +0800

    chore: update docs

---
+++ assets/dynamic.CUpF6Au6.js
@@ -0,0 +1,1 @@
+function t(t,i,...n){"function"==typeof this[i]&&this[i](t,...n)}export{t as p};
---
+++ assets/skyline.vTVdp_Ia.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./index-C_MfqTmT.js";const n={data:()=>({skylineRender:!1}),mounted(){this.skylineRender="skyline"===e().renderer}};export{n as S};
