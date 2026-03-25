e0533a78734b9207ab94e4f16b7110cfe1e2cd4e
novlan1 <1576271227@qq.com>
2026-03-25 12:10:16 +0800

    deploy: 2026/3/25 12:10:16

--- assets/theme-change.BUBvWjfx.js
+++
@@ -1,1 +0,0 @@
-import{a0 as t,a1 as e}from"./index-N29Cx-VX.js";const h={data:()=>({theme:"light"}),mounted(){this.theme=t.theme,"function"==typeof e&&e((t=>{this.theme=t.theme}))}};export{h as t};
