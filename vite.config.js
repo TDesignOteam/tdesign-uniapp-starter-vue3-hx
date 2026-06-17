bd6d43a7088e83ea43651438f02ed2cdd5ef464c
novlan1 <1576271227@qq.com>
2026-06-17 17:36:22 +0800

    feat: add vconsole

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 import path from 'path';
+import {
+  genVersionMpVitePlugin,
+  genVersionWebVitePlugin,
+} from '@plugin-light/vite-plugin-gen-version/lib/index.js';
+import { BUILD_NAME_MAP } from 't-comm/lib/v-console/config';
+
+const diffPlugins = [];
+
+if (process.env.UNI_PLATFORM !== 'h5') {
+  diffPlugins.push(genVersionMpVitePlugin());
+} else {
+  diffPlugins.push(genVersionWebVitePlugin({
+    buildName: BUILD_NAME_MAP.build,
+    commitName: BUILD_NAME_MAP.commit,
+    delay: 0,
+  }));
+}
 
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(), 
+    ...diffPlugins
+  ],
   resolve: {
     alias: {
     },
