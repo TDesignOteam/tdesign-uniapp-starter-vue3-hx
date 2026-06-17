242b2ae6aaa14e504ae5d5eb08b718f17d85cc86
novlan1 <1576271227@qq.com>
2026-06-17 16:06:10 +0800

    deploy: 2026/6/17 16:06:10

--- assets/theme-change.BXJCjihK.js
+++
@@ -1,1 +0,0 @@
-import{Z as t,$ as e}from"./index-CfhBEwIl.js";const h={data:()=>({theme:"light"}),mounted(){this.theme=t.theme,"function"==typeof e&&e((t=>{this.theme=t.theme}))}};export{h as t};
--- assets/uni-app.es.BoaDH38T.js
+++
@@ -1,1 +0,0 @@
-import"./index-CfhBEwIl.js";function r(r,t){return"string"==typeof r?t:r}export{r};
