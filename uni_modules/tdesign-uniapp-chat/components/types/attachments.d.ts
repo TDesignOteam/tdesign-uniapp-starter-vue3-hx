ecdb848d3018213ccaa7c1a90ece3a13fad1b9ec
novlan1 <1576271227@qq.com>
2026-08-06 11:55:26 +0800

    feat: update types

--- uni_modules/tdesign-uniapp-chat/components/types/attachments.d.ts
+++ uni_modules/tdesign-uniapp-chat/components/types/attachments.d.ts
@@ -1,5 +1,5 @@
-import type { TdAttachmentsProps } from '../attachments/type';
 import type { TransformEventHandlers, ExtractNonOnProps } from '../../../tdesign-uniapp/components/common/common';
+import type { TdAttachmentsProps } from '../attachments/type';
 
 export type AttachmentsProps = ExtractNonOnProps<TdAttachmentsProps>;
 export type AttachmentsEmits = TransformEventHandlers<TdAttachmentsProps, true>;
